c659e0ba4851b248f0846107a166f7d1505e11f8
James Bell <james@james-bell.co.uk>
2015-01-01 21:55:34 +0000

    MAJOR UPDATE: Many things have changed in this release and I need to do an updated readme to detail the changes. Scrrenshots are now handled but node-webshot which means the external snap.js file is no longer required. Image comparison is done via resemble.js so the dependecy of Imagemagick is no longer required. Custom logging has been implemented with the option to hide it. General tidy up has taken place including linting with JSHint. Probably more too! Updated readme coming very soon (hopefully tomorrow). Happy new year :)

--- cli.js
+++ cli.js
@@ -1,19 +1,36 @@
 #!/usr/bin/env node
 'use strict';
 
-var program = require('commander'),
-	chalk  = require('chalk'),
-	wraith = require('./index');
+var moment  = require('moment');
+var program = require('commander');
+var log     = require('./lib/logger');
 
-	program.unknownOption = program.help;
-	program
-		.usage('[options] <file ...>')
-		.version(require('./package').version)
-		.option('-c, --config [config]', 'You must specifiy a configuration file')
-		.parse(process.argv);
+program.unknownOption = program.help;
+program
+	.usage('[options] <file ...>')
+	.version(require('./package').version)
+	.option('-c, --config [config]', 'You must specifiy a configuration file')
+	.option('-q, --quiet', 'All logging is hidden')
+	.parse(process.argv);
 
 if(!program.config) {
 	program.help();
 } else {
-	wraith.run(program.config);
+	var wraith = require('./lib/wraith');
+	var config = require(program.config);
+	var timer  = false;
+
+	if(program.quiet) {
+		config.quiet = true;
+	} else {
+		timer = moment();
+	}
+
+	new wraith(config, function() {
+		if(timer) {
+			log.info('Time Taken: ' + moment().diff(timer, 'seconds') + 's');
+		}
+		log.success('Done');
+	});
+
 }
